e33794f84b17e48659eca391fef26c3dc00368c6
federico-priolo <57253933+federico-priolo@users.noreply.github.com>
2020-02-29 21:21:37 +0100

    Add files via upload

--- tools/windows/menu.bat
+++ tools/windows/menu.bat
@@ -3,7 +3,8 @@ cls
 echo cobagar    compila agarcob.cbl e genera agarcob.dll per la produzione
 echo animagar   anima agarcob.cbl 
 echo cobgui     compila un .cbl e genera e compila il .gui formato .exe  (default per GUICOBOL)
-echo animgui    anima  un .cbl (default per ANIMATE GUICOBOL)
+echo animate    anima  un .cbl (default per GUICOBOL)
+echo anigui     anima  un .gui 
 echo cobcbl     compila un. cbl con le librerie agar senza preprocesso GUICOBOL
 echo animcbl    anima un .cbl  senza preprocesso GUICOBOL
 
